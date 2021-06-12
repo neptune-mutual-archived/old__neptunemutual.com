8d3de81914f9f6ab4f3fa91e40101c1e474d64af
flashburst <82953782+flashburst@users.noreply.github.com>
2021-06-12 15:16:15 +0530

    Remove bounty promo

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,23 +2,6 @@ import * as React from 'react'
 import { LanguageProvider } from './src/i18n'
 import './src/styles/global.css'
 
-export const onRenderBody = ({ setPostBodyComponents }) => {
-  setPostBodyComponents([
-    <script
-      key='gleam#1'
-      dangerouslySetInnerHTML={{
-        __html: `(function(d, t){
-        if(!(window.location.hash!='#gleam'&&(''+document.cookie).match(/(^|;)\s*Gleamz68kq=X($|;)/))) {
-            var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-            g.setAttribute("defer", "");
-            g.src = "https://widget.gleamjs.io/z68kq/ol.js"; s.parentNode.insertBefore(g, s);
-        }}
-        (document, "script"));`
-      }}
-    />
-  ])
-}
-
 export const wrapRootElement = ({ element }) => (
   <LanguageProvider>{element}</LanguageProvider>
 )
