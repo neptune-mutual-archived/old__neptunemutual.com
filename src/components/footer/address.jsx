e458af3ec8b5cdfc2747ae483e5a959d00126fc3
flashburst <82953782+flashburst@users.noreply.github.com>
2021-12-31 09:23:26 +0530

    Added address

---
+++ src/components/footer/address.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+export const Address = () => {
+  return (
+    <>
+      <div className='pt-4 mt-4 border-t border-gray-700'>
+        <address className='text-center text-sm my-6'>
+          <strong className='font-bold'>Postal Address: </strong>Neptune Mutual
+          C/O Chain Commit Ltd, Athenia House, 10-14 Andover Road, Winchester,
+          SO23 7BS, United Kingdom
+        </address>
+      </div>
+    </>
+  )
+}
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,14 +1,16 @@
 import * as React from 'react'
+import { Address } from './address'
 import { Content } from './content'
 import { Links } from './links'
 
 export const Footer = () => {
   return (
-    <div className='flex justify-center pt-12 md:pt-24 pb-12 md:pb-24'>
+    <footer className='flex justify-center pt-12 md:pt-24 pb-12 md:pb-24'>
       <div className='w-full max-w-screen-2xl px-6 md:px-8 text-base md:text-lg text-gray-400'>
         <Links />
+        <Address />
         <Content />
       </div>
-    </div>
+    </footer>
   )
 }
